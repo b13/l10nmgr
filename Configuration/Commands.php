6fe760fc900de1b9ae9219b7060d410ad0725b17
Michael Giek <michael.giek@b13.de>
2018-10-15 14:50:56 +0200

    [TASK] Change misspelling

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -3,7 +3,7 @@ return [
     'l10nmanager:export' => [
         'class' => \Localizationteam\L10nmgr\Command\Export::class,
     ],
-    'l10nmanger:import' => [
+    'l10nmanager:import' => [
         'class' => \Localizationteam\L10nmgr\Command\Import::class,
     ],
 ];
